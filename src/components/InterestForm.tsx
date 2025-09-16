8138e7f5859f47497c4e1f72b4241b871cd5a653
22-warabi <hasegawa.s.an@m.titech.ac.jp>
2025-09-16 19:40:22 +0900

    add an interest form without actual form implementation

---
+++ src/components/InterestForm.tsx
@@ -0,0 +1,11 @@
+import styles from './InterestForm.module.css';
+
+export const InterestForm = () => {
+    return (
+        <div className={styles.interestFormWrapper}>
+            <h2 className={styles.heading}>ご興味のある方へ</h2>
+            <p>Qiskit Fall Festについての最新情報を受け取るために、以下のフォームにメールアドレスを入力してください</p>
+            <p>form comming soon...</p>
+        </div>
+    )
+}
