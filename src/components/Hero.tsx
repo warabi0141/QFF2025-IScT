c46f205690590940631f9cc24af06e18f21ef40d
22-warabi <hasegawa.s.an@m.titech.ac.jp>
2025-09-16 14:53:39 +0900

    add Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,13 @@
 import styles from './Hero.module.css';
+import qffLogo from '../assets/Badge_Dark.svg';
+import heroBackground from '../assets/HeroBackground.svg';
 
 export const Hero = () => {
     return (
-        <div></div>
+        <div className = {styles.hero}>
+            <img src={qffLogo} alt="QFF Logo" className={styles['svg-logo']} />
+            <img src={heroBackground} alt="Hero Background" className={styles['svg-bg']} />
+            <p className={styles['hero-text']}>@ScienceTokyo</p>
+        </div>
     )
 }
