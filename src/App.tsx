72bafdccbae44e46d56983c64d1c1408af071483
22-warabi <hasegawa.s.an@m.titech.ac.jp>
2025-09-16 18:50:59 +0900

    add footer text

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Hero } from './components/Hero'
 import { About } from './components/About'
+import { Footer } from './components/Footer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Hero />
       <About />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+    return (
+        <div className={styles.footerWrapper}>
+            <p className={styles.footerText}>© 2025 organizing committee at Science Tokyo. All Rights Reserved.</p>
+        </div>
+    )
+}
