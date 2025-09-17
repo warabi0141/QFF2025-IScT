a19368deaf9fd233e62c0c757b7d1a3751fa849a
22-warabi <hasegawa.s.an@m.titech.ac.jp>
2025-09-17 22:35:08 +0900

    fix InterestForm to change the selector with text field

--- src/components/InterestForm.tsx
+++ src/components/InterestForm.tsx
@@ -32,7 +32,7 @@ export const InterestForm = () => {
         <div className={styles.interestFormWrapper}>
             <h2 className={styles.heading}>ご興味のある方へ</h2>
             <p>開催場所の詳細やワークショップの資料を受け取るためにSlackのグループに招待します</p>
-            <p>お名前と東京科学大学のmアドレス、所属している系を選択して送信してください</p>
+            <p>お名前と東京科学大学のmアドレス、所属している系を記入して送信してください</p>
             {state.succeeded ? (
                 <>
                     <p>ご登録いただきありがとうございます！</p>
@@ -74,23 +74,17 @@ export const InterestForm = () => {
 
                             <Field.Root>
                                 <Field.Label>所属系</Field.Label>
-                                <Field.HelperText>所属している系を選択してください</Field.HelperText>
-                                <NativeSelect.Root
+                                <Field.HelperText>所属している系を記入してください。Slackのユーザー検索に利用します</Field.HelperText>
+                                <Input 
+                                    id="department"
+                                    type="text"
+                                    name="department"
+                                    value={formData.department}
+                                    onChange={handleChange}
+                                    required
                                     maxWidth={"500px"}
                                     border="solid 1px #ccc"
-                                    >
-                                    <NativeSelect.Field 
-                                    placeholder="所属系を選択" name="department" value={formData.department} onChange={handleChange}
-                                    >
-                                        <For each={["数学系", "物理学系", "化学系", "地球惑星科学系", "機械系", "システム制御系", "電気電子系", "情報通信系", "経営工学系", "材料系", "応用化学系", "数理計算科学系", "情報理工学系", "生命理工学系", "建築学系", "土木・環境工学系", "融合理工学系", "その他"]}>
-                                            {(item) => (
-                                                <option key={item} value={item}>
-                                                    {item}
-                                                </option>
-                                            )}
-                                        </For>
-                                    </NativeSelect.Field>
-                                </NativeSelect.Root>
+                                />
                             </Field.Root>
                         </Fieldset.Content>
                     </Fieldset.Root>
